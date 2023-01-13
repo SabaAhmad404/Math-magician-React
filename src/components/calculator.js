4a6cef432159999594578a260b8e07d24475e06e
Saba Ahmad <sabaahmad895@gmail.com>
2023-01-14 02:43:28 +0500

    refacter component into funtion

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,199 +1,192 @@
-
-import React, { useState } from 'react';
+import { useState } from 'react';
 import calculate from '../logic/calculate';
-import { count } from 'console';
 
-// export default class Calculator extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { total: 0, next: null, operation: null };
-//   }
-const Calculator=()=>{
-    const [counter, setCount]=useState({ total: 0, next: null, operation: null });
-};
- const buttonClicked = (event) => {
+const Calculator = () => {
+  const [display, newDisplay] = useState({
+    total: 0,
+    next: null,
+    operation: null,
+  });
+
+  const buttonClicked = (event) => {
     const btnName = event.target.innerText;
-    const answer = calculate(this.state, btnName);
-    this.setState(answer);
-    const answer = calculate(counter, btnName);
-    setCount(answer);
+    const answer = calculate(display, btnName);
+    newDisplay(answer);
   };
-  const { total, next, operation} = counter;
+  const { total, next, operation } = display;
 
-  render() {
-    const { total, operation, next } = this.state;
-    return (
-      <div className="container">
-        <div className="zero">
-          {total}
-          {operation}
-          {next}
+  return (
+    <div className="container">
+      <div className="zero">
+        {total}
+        {operation}
+        {next}
+      </div>
+      <div className="line1">
+        <button
+          type="button"
+          className="clearAll"
+          value="all-clear"
+          onClick={buttonClicked}
+        >
+          AC
+        </button>
+        <button
+          type="button"
+          className="plus"
+          value="+/-"
+          onClick={buttonClicked}
+        >
+          +/-
+        </button>
+        <button
+          type="button"
+          className="percentage"
+          value="%"
+          onClick={buttonClicked}
+        >
+          %
+        </button>
+        <button
+          type="button"
+          className="divide"
+          value="/"
+          onClick={buttonClicked}
+        >
+          {' '}
+          &divide;
+        </button>
+        <div className="line2">
+          <button
+            type="button"
+            className="seven"
+            value="7"
+            onClick={buttonClicked}
+          >
+            7
+          </button>
+          <button
+            type="button"
+            className="eight"
+            value="8"
+            onClick={buttonClicked}
+          >
+            8
+          </button>
+          <button
+            type="button"
+            className="nine"
+            value="9"
+            onClick={buttonClicked}
+          >
+            9
+          </button>
+          <button
+            type="button"
+            className="ex"
+            value="x"
+            onClick={buttonClicked}
+          >
+            x
+          </button>
         </div>
-        <div className="line1">
+        <div className="line3">
           <button
             type="button"
-            className="clearAll"
-            value="all-clear"
-            onClick={this.buttonClicked}
+            className="four"
+            value="4"
+            onClick={buttonClicked}
           >
-            AC
+            4
           </button>
           <button
             type="button"
-            className="plus"
-            value="+/-"
-            onClick={this.buttonClicked}
+            className="five"
+            value="5"
+            onClick={buttonClicked}
           >
-            +/-
+            5
           </button>
           <button
             type="button"
-            className="percentage"
-            value="%"
-            onClick={this.buttonClicked}
+            className="six"
+            value="6"
+            onClick={buttonClicked}
           >
-            %
+            6
           </button>
           <button
             type="button"
-            className="divide"
-            value="/"
-            onClick={this.buttonClicked}
+            className="minus"
+            value="-"
+            onClick={buttonClicked}
           >
-            {' '}
-            &divide;
+            -
           </button>
-          <div className="line2">
-            <button
-              type="button"
-              className="seven"
-              value="7"
-              onClick={this.buttonClicked}
-            >
-              7
-            </button>
-            <button
-              type="button"
-              className="eight"
-              value="8"
-              onClick={this.buttonClicked}
-            >
-              8
-            </button>
-            <button
-              type="button"
-              className="nine"
-              value="9"
-              onClick={this.buttonClicked}
-            >
-              9
-            </button>
-            <button
-              type="button"
-              className="ex"
-              value="x"
-              onClick={this.buttonClicked}
-            >
-              x
-            </button>
-          </div>
-          <div className="line3">
-            <button
-              type="button"
-              className="four"
-              value="4"
-              onClick={this.buttonClicked}
-            >
-              4
-            </button>
-            <button
-              type="button"
-              className="five"
-              value="5"
-              onClick={this.buttonClicked}
-            >
-              5
-            </button>
-            <button
-              type="button"
-              className="six"
-              value="6"
-              onClick={this.buttonClicked}
-            >
-              6
-            </button>
-            <button
-              type="button"
-              className="minus"
-              value="-"
-              onClick={this.buttonClicked}
-            >
-              -
-            </button>
-          </div>
-          <div className="line4">
-            <button
-              type="button"
-              className="one"
-              value="1"
-              onClick={this.buttonClicked}
-            >
-              1
-            </button>
-            <button
-              type="button"
-              className="two"
-              value="2"
-              onClick={this.buttonClicked}
-            >
-              2
-            </button>
-            <button
-              type="button"
-              className="three"
-              value="3"
-              onClick={this.buttonClicked}
-            >
-              3
-            </button>
-            <button
-              type="button"
-              className="plus-sign"
-              value="+"
-              onClick={this.buttonClicked}
-            >
-              +
-            </button>
-          </div>
-          <div className="line5">
-            <button
-              type="button"
-              className="null"
-              value="0"
-              onClick={this.buttonClicked}
-            >
-              0
-            </button>
-            <button
-              type="button"
-              className="point"
-              value="."
-              onClick={this.buttonClicked}
-            >
-              .
-            </button>
-            <button
-              type="button"
-              className="equal"
-              value="="
-              onClick={this.buttonClicked}
-            >
-              =
-            </button>
-          </div>
+        </div>
+        <div className="line4">
+          <button
+            type="button"
+            className="one"
+            value="1"
+            onClick={buttonClicked}
+          >
+            1
+          </button>
+          <button
+            type="button"
+            className="two"
+            value="2"
+            onClick={buttonClicked}
+          >
+            2
+          </button>
+          <button
+            type="button"
+            className="three"
+            value="3"
+            onClick={buttonClicked}
+          >
+            3
+          </button>
+          <button
+            type="button"
+            className="plus-sign"
+            value="+"
+            onClick={buttonClicked}
+          >
+            +
+          </button>
+        </div>
+        <div className="line5">
+          <button
+            type="button"
+            className="null"
+            value="0"
+            onClick={buttonClicked}
+          >
+            0
+          </button>
+          <button
+            type="button"
+            className="point"
+            value="."
+            onClick={buttonClicked}
+          >
+            .
+          </button>
+          <button
+            type="button"
+            className="equal"
+            value="="
+            onClick={buttonClicked}
+          >
+            =
+          </button>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+};
 
-export{Calculator as default}
+export default Calculator;
