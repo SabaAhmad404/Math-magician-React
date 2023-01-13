1c84c31450459e5d4d6a86676ceb75bfff98246d
Saba Ahmad <sabaahmad895@gmail.com>
2023-01-13 22:38:29 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Calculator from './components/calculator';
 
-// eslint-disable-next-line react/prefer-stateless-function
+
 class App extends Component {
   render() {
     return (
