b989608aae8d9d68c0653949bec5e0f742afa902
Saba Ahmad <sabaahmad895@gmail.com>
2023-01-13 23:23:49 +0500

    some lint erros

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
-import Calculator from './components/calculator';
+import React, { Component } from "react";
+import Calculator from "./components/calculator";
 
-// eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
   render() {
     return (
